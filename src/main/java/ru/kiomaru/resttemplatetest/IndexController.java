ebce47be7335545bb2097a097a6633c6fb459c8a
ekizilov <alias5994@yandex.ru>
2024-10-07 08:55:13 +0300

    SolvedTask

--- src/main/java/ru/kiomaru/resttemplatetest/IndexController.java
+++ src/main/java/ru/kiomaru/resttemplatetest/IndexController.java
@@ -1,8 +1,12 @@
 package ru.kiomaru.resttemplatetest;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class IndexController {
@@ -10,14 +14,21 @@ public class IndexController {
     final private UserService userService;
 
     public static String answer = "";
+    public static String url = "http://94.198.50.185:7081/api/users";
+    private final RestTemplate restTemplate;
 
-    public IndexController(UserService userService) {
+    public IndexController(UserService userService, RestTemplate restTemplate) {
         this.userService = userService;
+        this.restTemplate = restTemplate;
     }
 
     @GetMapping("/")
     public String index() {
         System.out.println(userService.getUsers());
+        userService.performRequestWithSession(url, userService.createUser(), HttpMethod.POST);
+        userService.performRequestWithSession(url, userService.updateUser(), HttpMethod.PUT);
+        userService.performRequestWithSession(url + "/3", HttpMethod.DELETE);
+        System.out.println(answer);
         return "index";
     }
 }
--- src/main/java/ru/kiomaru/resttemplatetest/UserService.java
+++ src/main/java/ru/kiomaru/resttemplatetest/UserService.java
@@ -1,6 +1,7 @@
 package ru.kiomaru.resttemplatetest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +17,7 @@ import java.util.Objects;
 public class UserService {
 
     final RestTemplate restTemplate;
-    private String sessionId;
+    public static String sessionId;
 
 
     public UserService(RestTemplate restTemplate) {
@@ -24,9 +25,8 @@ public class UserService {
     }
 
     public List<User> getUsers() {
-        String url = "http://94.198.50.185:7081/api/users";
         ResponseEntity<User[]> responseEntity = restTemplate.exchange(
-                url,
+                IndexController.url,
                 HttpMethod.GET,
                 null,
                 User[].class
@@ -41,4 +41,49 @@ public class UserService {
         return Arrays.asList(Objects.requireNonNull(responseEntity.getBody()));
     }
 
+    public String performRequestWithSession(String uri, Object body, HttpMethod method) {
+        if (sessionId == null) {
+            throw new IllegalStateException("SessionId not set");
+        }
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.COOKIE, sessionId);
+        headers.add(HttpHeaders.CONTENT_TYPE, "application/json");
+        HttpEntity<User> entity = new HttpEntity<>((User) body ,headers);
+        ResponseEntity<String> response = restTemplate.exchange(uri, method, entity, String.class);
+        System.out.println(response.getHeaders());
+        System.out.println(response.getBody());
+        IndexController.answer+=response.getBody();
+        return response.getBody();
+    }
+
+    public String performRequestWithSession(String uri, HttpMethod method) {
+        if (sessionId == null) {
+            throw new IllegalStateException("SessionId not set");
+        }
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.COOKIE, sessionId);
+        headers.add(HttpHeaders.CONTENT_TYPE, "application/json");
+        HttpEntity<User> entity = new HttpEntity<>(headers);
+        ResponseEntity<String> response = restTemplate.exchange(uri, method, entity, String.class);
+        System.out.println(response.getHeaders());
+        System.out.println(response.getBody());
+        IndexController.answer+=response.getBody();
+        return response.getBody();
+    }
+
+    public User createUser() {
+        User user = new User();
+        user.setId(3L);
+        user.setName("James");
+        user.setLastName("Brown");
+        user.setAge((byte) 20);
+        return user;
+    }
+
+    public Object updateUser() {
+        User user = createUser();
+        user.setName("Thomas");
+        user.setLastName("Shelby");
+        return user;
+    }
 }
